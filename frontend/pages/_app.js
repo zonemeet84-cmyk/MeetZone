ee933fc94f09d0df121b55fa787561e6d1415a25
kawalsahab723-ops <kawalsahab723@gmail.com>
2026-05-21 17:42:17 +0530

    Add global favicon and update favicon.png

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     <SessionProvider session={session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
