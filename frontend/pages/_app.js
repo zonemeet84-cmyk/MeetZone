e6373428f710d2a21cfcb3b6a23c5c09d51bd95a
kawalsahab723-ops <kawalsahab723@gmail.com>
2026-06-01 16:21:40 +0530

    fix: add favicon.ico to resolve 404 at /favicon.ico
    - Converted existing favicon.png to favicon.ico using png-to-ico
    - Added <link rel='icon' href='/favicon.ico'> in _app.js
    - Kept existing favicon.png link for modern browsers
    - Structured data logo URL now resolves correctly

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -74,6 +74,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       `}</style>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.png" type="image/png" />
       </Head>
       <Component {...pageProps} />
